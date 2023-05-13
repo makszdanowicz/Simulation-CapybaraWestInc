d4d33b9242dcccdb6ded048b2904aa7e9c49f089
justyna <276587@student.pwr.edu.pl>
2023-05-13 19:55:31 +0200

    test: map initialization

--- src/main/java/capybarawest/inc/Main.java
+++ src/main/java/capybarawest/inc/Main.java
@@ -1,9 +1,38 @@
 package capybarawest.inc;
 
+import java.util.Scanner;
+
 public class Main {
+    private String[][] map;
+    private int size;
+
+    public Main(int size){
+        this.size = size;
+    }
+    public void map_initialization(){
+        map = new String[size][size];
+        for (int i = 0; i < size; i++){
+            for(int j = 0; j < size; j++){
+                map[i][j] = "0";
+            }
+        }
+    }
+    public void print_map(){
+        for(int i = 0; i < size; i++){
+            for(int j = 0; j < size; j++){
+                System.out.print(map[i][j] + " ");
+            }
+            System.out.println(" ");
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("Hello simulation!");
-        System.out.println("Dupa 2");
+        int size;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Podaj rozmiar mapy: ");
+        size = scan.nextInt();
+        Main object = new Main(size);
+        object.map_initialization();
+        object.print_map();
     }
 
 }

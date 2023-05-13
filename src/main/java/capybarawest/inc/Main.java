d74af393e8996c5e42eba0d636f314b75ac3bb89
justyna <276587@student.pwr.edu.pl>
2023-05-13 17:31:36 +0200

    added message from second developer

--- src/main/java/capybarawest/inc/Main.java
+++ src/main/java/capybarawest/inc/Main.java
@@ -3,5 +3,7 @@ package capybarawest.inc;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello simulation!");
+        System.out.println("Dupa 2");
     }
+
 }

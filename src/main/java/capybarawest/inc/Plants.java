343a67559fd8e40310a85aeda882c0d2be970a57
justyna <276587@student.pwr.edu.pl>
2023-05-13 17:57:49 +0200

    added abstract class Plants

---
+++ src/main/java/capybarawest/inc/Plants.java
@@ -0,0 +1,18 @@
+package capybarawest.inc;
+
+public abstract class Plants {
+    protected double hp;
+    protected int koordynata_ox;
+    protected int koordynata_oy;
+    public Plants(){
+        this.hp = -1111111111;
+        this.koordynata_ox = -11111111;
+        this.koordynata_oy = -1111111111;
+    }
+    public Plants(double hp, int koordynata_ox, int koordynata_oy){
+        this.hp = hp;
+        this.koordynata_ox = koordynata_ox;
+        this.koordynata_oy = koordynata_oy;
+    }
+}
+

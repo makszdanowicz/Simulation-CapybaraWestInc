0e0dbd9808f48988388dc4afd60396f15bb49a8b
makszdanowicz <273564@student.pwr.edu.pl>
2023-05-13 18:02:59 +0200

    added class Capybara

---
+++ src/main/java/capybarawest/inc/Capybara.java
@@ -0,0 +1,11 @@
+package capybarawest.inc;
+
+public class Capybara extends Animal{
+    public Capybara(){
+        super();
+    }
+
+    public Capybara(double hp, int koordynata_ox, int koordynata_oy, int speed, int damage){
+        super(hp,koordynata_ox,koordynata_oy,speed,damage);
+    }
+}

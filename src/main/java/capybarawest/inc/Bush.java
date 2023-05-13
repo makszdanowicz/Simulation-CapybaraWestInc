18afe95b3b0183d32e26eb9520f784136a20c5a9
justyna <276587@student.pwr.edu.pl>
2023-05-13 19:03:56 +0200

    added class Bush

---
+++ src/main/java/capybarawest/inc/Bush.java
@@ -0,0 +1,10 @@
+package capybarawest.inc;
+
+public class Bush extends Plants{
+    public Bush(){
+        super();
+    }
+    public Bush( double hp, int koordynata_ox, int koordynata_oy){
+        super(hp, koordynata_ox, koordynata_oy);
+    }
+}

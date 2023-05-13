ecfb7df877553da67b4628a0722268e4a51720b5
makszdanowicz <273564@student.pwr.edu.pl>
2023-05-13 18:16:41 +0200

    added class Dog

---
+++ src/main/java/capybarawest/inc/Dog.java
@@ -0,0 +1,11 @@
+package capybarawest.inc;
+
+public class Dog extends Animal{
+    public Dog(){
+        super();
+    }
+
+    public Dog(double hp, int koordynata_ox, int koordynata_oy, int speed, int damage){
+        super(hp, koordynata_ox, koordynata_oy, speed, damage);
+    }
+}

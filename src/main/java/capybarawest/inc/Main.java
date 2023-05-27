6afcde42a9f161ad1dac6d90f17dcbd787a62870
justyna <276587@student.pwr.edu.pl>
2023-05-27 15:44:21 +0200

    add bush

--- src/main/java/capybarawest/inc/Main.java
+++ src/main/java/capybarawest/inc/Main.java
@@ -35,12 +35,15 @@ public class Main {
     public static void main(String[] args) {
         int size;
         int tree_percentage;
+        int bush_persentage;
         Scanner scan = new Scanner(System.in);
         System.out.println("Podaj rozmiar mapy(liczba wierszy i liczba kolumn): ");
         size = scan.nextInt();
         System.out.println("Podaj ilość drzew(w procentach od 0 do 100): ");
         tree_percentage = scan.nextInt();
-        Simulation symulacja = new Simulation(size, tree_percentage);
+        System.out.println("Podaj ilość krzaków(w procentach od 0 do " + (100-tree_percentage) + "): ");
+        bush_persentage = scan.nextInt();
+        Simulation symulacja = new Simulation(size, tree_percentage, bush_persentage);
         //Main object = new Main(size);
         symulacja.map_initialization();
         symulacja.print_map();
--- src/main/java/capybarawest/inc/Simulation.java
+++ src/main/java/capybarawest/inc/Simulation.java
@@ -1,35 +1,54 @@
 package capybarawest.inc;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 public class Simulation {
     private int size;
     private int tree_persentage;
+    private int bush_persentage;
     private String[][] map;
-    public Simulation(int size, int tree_persentage){
+    //Map<Integer, Capybara> capybaraMap = new HashMap<>();
+    //Map<Integer, Dog> dogMap = new HashMap<>();
+    Map<Integer, Tree> treeMap = new HashMap<>();
+   // Map<Integer, Bush> bushMap = new HashMap<>();
+    public Simulation(int size, int tree_persentage, int bush_persentage){
         this.size = size;
         this.tree_persentage = tree_persentage;
+        this.bush_persentage = bush_persentage;
     }
     public void map_initialization(){
         map = new String[size][size];
         Random rand = new Random();
-        for (int i = 0; i < size; i++){
-            for(int j = 0; j < size; j++){
-                if(i == 0)
-                {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (i == 0) {
                     map[0][j] = "W";
-                }
-                else{
+                } else {
                     int random = rand.nextInt(101);
-                    if (random <= tree_persentage){
+                    if (random <= tree_persentage) {
                         map[i][j] = "T";
-                    }
-                    else{
+
+                    } else {
                         map[i][j] = "0";
                     }
                 }
             }
         }
+            for (int i = 1; i < size; i++){
+                for(int j = 0; j < size; j++){
+                    if(map[i][j] == "0"){
+                        int random1 = rand.nextInt(101);
+                        if(random1 <= bush_persentage){
+                            map[i][j] = "B";
+                        }
+                        else{
+                            map[i][j] = "0";
+                        }
+                    }
+                }
+            }
     }
     public void print_map(){
         for(int i = 0; i < size; i++){
@@ -39,6 +58,9 @@ public class Simulation {
                 }
                 else if(map[i][j] == "T"){
                     System.out.print(ColorClass.GREEN_BOLD + map[i][j] + " ");
+                }
+                else if (map[i][j] == "B"){
+                    System.out.print(ColorClass.PURPLE_BOLD + map[i][j] + " ");
                 }
                 else{
                     System.out.print(ColorClass.WHITE + map[i][j] + " ");
